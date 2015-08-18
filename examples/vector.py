417484993f78b347a524670290182b2f9f17840c
Vinhthuy Phan <vphan@memphis.edu>
2015-08-18 14:57:46 -0500

    .

--- examples/vector.py
+++ examples/vector.py
@@ -1,6 +1,17 @@
+import math
+
 class Vector:
    counter = 0
    def __init__(self, u,v):
       self.u = u
       self.v = v
       Vector.counter += 1
+
+   def norm(self):
+   	return math.sqrt(self.u*self.u + self.v*self.v)
+
+
+
+x = Vector(3,4)
+print(x.u, x.v)
+print(x.norm())
