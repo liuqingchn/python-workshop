06602bc8523ea0eb29c11e75b96f86724c666c8f
vtphan <vphan@memphis.edu>
2015-08-18 21:38:48 -0500

    .

--- data/test_seaborn.py
+++ data/test_seaborn.py
@@ -1,30 +1,33 @@
 
-import matplotlib.pyplot as plt
-from pandas import read_csv
-import seaborn as sns
+import matplotlib.pyplot as plot
+import pandas
+import seaborn
 
 # Data
-crimes = read_csv("crimeRatesByState2005.csv")
-mpg = read_csv("mpg.csv")
+crimes = pandas.read_csv("crimeRatesByState2005.csv")
+mpg = pandas.read_csv("mpg.csv")
 
 # Plot of distribution, one variable
-sns.distplot(crimes["robbery"])
+seaborn.distplot(crimes["robbery"])
 
 # Save figure and then clear plot
-plt.savefig("robbery.png"); plt.clf()
+plot.savefig("robbery.png")
+plot.clf()
 
 
 # Plot of 2 numerical variables
-sns.jointplot(x=crimes["robbery"], y=crimes["murder"])
+seaborn.jointplot(x=crimes["robbery"], y=crimes["murder"])
 
 # Save figure and then clear plot
-plt.savefig("robbery_murder.png"); plt.clf()
+plot.savefig("robbery_murder.png")
+plot.clf()
 
 
 
 # Plot of a categorical (x) versus a numerical (y)
-sns.boxplot(x=mpg["class"], y=mpg["cty"])
+seaborn.boxplot(x=mpg["class"], y=mpg["cty"])
 
 # Save figure and then clear plot
-plt.savefig("class_cty.png"); plt.clf()
+plot.savefig("class_cty.png")
+plot.clf()
 
