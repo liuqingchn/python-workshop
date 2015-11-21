9c64d2bbe862401412ef6a01389ba9e6b0c557b2
vtphan <vphan@memphis.edu>
2015-11-21 13:27:02 -0600

    add logistic regression using statsmodel

--- 2016/Logistic-regression/lr.py
+++ 2016/Logistic-regression/lr.py
@@ -22,4 +22,18 @@ from sklearn.metrics import accuracy_score
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 lr = LogisticRegression()
 lr.fit(X_train, y_train)
-print(accuracy_score(lr.predict(X_test), y_test))
+print("Accuracy of logistic regression (skilearn):",
+	accuracy_score(lr.predict(X_test), y_test))
+
+
+## Using statsmodels
+print("\nNow we will use statsmodels to do logistic regression.\n")
+import statsmodels.formula.api as smf
+Xy_train = pandas.DataFrame.join(X_train, y_train)
+model = smf.logit(formula='Survived ~ Age + SibSp + Fare', data=Xy_train)
+result = model.fit()
+print(result.summary())
+predicted = result.predict(X_test)
+print("Predicted probabilities", predicted)
+print("\n\nAccuracy based on predicted probabilities:",
+	accuracy_score([ 1 if p > 0.5 else 0 for p in predicted ], y_test))
