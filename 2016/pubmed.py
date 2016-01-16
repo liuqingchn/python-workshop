6a2168a26e4fe6b1eeb70d81c3e2302be8bab262
vtphan <vphan@memphis.edu>
2016-01-16 16:35:52 -0600

    .

--- 2016/pubmed.py
+++ 2016/pubmed.py
@@ -24,11 +24,39 @@ class Pubmed:
 		self.root = r
 		self.articles = r.findall('./PubmedArticle/MedlineCitation/Article')
 
+	def ids(self):
+		t = []
+		for i in self.root.findall('./PubmedArticle/MedlineCitation/PMID'):
+			t.append(i.text)
+		return t
+
 	def titles(self):
 		t = []
 		for i in self.articles:
 			t.append(i.find('./ArticleTitle').text)
 		return t
+
+	def abstracts(self):
+		t = []
+		for i in self.articles:
+			a = i.findall('./Abstract/AbstractText')
+			b = '\n'.join([ j.text for j in a ])
+			t.append(b)
+		return t
+
+	def journals(self):
+		t = []
+		for i in self.articles:
+			t.append(i.find('./Journal/Title').text)
+		return t
+
+	# homework
+	def authors(self):
+		return []
+
+	def date(self):
+		return []
+
 
 
 
--- 2016/test_pubmed.py
+++ 2016/test_pubmed.py
@@ -1,4 +1,8 @@
 import pubmed
 
 res = pubmed.Search('breast cancer',10)
-print(res.titles())
+# print(res.titles())
+# print(res.journals())
+print(res.ids())
+abstracts = res.abstracts()
+print(abstracts[0])
